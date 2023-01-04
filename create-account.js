6f3cc0af0e07643f0f9999b63d3e86e7bd7e331d
Josh Darling <joshua.a.darling@gmail.com>
2023-01-03 20:10:57 -0700

    part 2 bank acoount complete

--- create-account.js
+++ create-account.js
@@ -1,4 +1,47 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+    let userPin = pin
+    let initialAmount = amount
+
+    return obj = {
+        checkBalance: function(insertPin){
+            if(insertPin === userPin){
+                return `$${initialAmount}`
+            } else{
+                return "Invalid PIN."
+            }
+        },
+        deposit: function(insertPin, insertAmount){
+            if(insertPin === userPin && !isNaN(insertAmount)){
+                initialAmount += insertAmount
+                return `Succesfully deposited $${insertAmount}. Current balance: $${initialAmount}.`
+
+            } else {
+                return "Invalid PIN."
+            }
+
+        },
+        withdraw: function(insertPin, withdraw){
+            if(withdraw > initialAmount) return "Withdrawal amount exceeds account balance. Transaction cancelled."
+            if(insertPin === userPin && !isNaN(withdraw)){
+                initialAmount -= withdraw
+                return `Succesfully withdrew $${withdraw}. Current balance: $${initialAmount}.`
+
+            } else{
+                return "Invalid PIN."
+            }
+        },
+        changePin: function(oldPin, newPin){
+            if(oldPin === userPin){
+                userPin = newPin
+                return "PIN successfully changed!"
+            } else {
+                return "Invalid PIN."
+
+            }
+        }
+
+    }
+
 
 }
 
