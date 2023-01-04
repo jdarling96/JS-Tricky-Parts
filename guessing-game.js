a0ccbab5da2e3e68a12d711f0fa4de1a3f5d5ca3
Josh Darling <joshua.a.darling@gmail.com>
2023-01-03 19:40:48 -0700

    part 2 complete using closure

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,23 @@
 function guessingGame() {
+  let winningNumber = Math.floor(Math.random() * 100);
 
+  let numberGuesses = 0;
+  let won = false;
+  return function (guess) {
+    numberGuesses++;
+    if (!won) {
+      if (guess === winningNumber) {
+        won = true;
+        return `You win! You found ${winningNumber} in ${numberGuesses} guesses.`;
+      } else if (guess > winningNumber) {
+        return `${guess} is too high!`;
+      } else {
+        return `${guess} is too low!`;
+      }
+    } else {
+      return "The game is over, you already won!";
+    }
+  };
 }
 
 module.exports = { guessingGame };
